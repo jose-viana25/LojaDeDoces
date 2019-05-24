46705a1ee89aedc57f52eac5fe58e7ebd9de5c16
Vitor1809 <42480077+Vitor1809@users.noreply.github.com>
2019-05-24 11:45:08 -0300

    Update DaoCliente.java

--- src/dao/DaoCliente.java
+++ src/dao/DaoCliente.java
@@ -35,6 +35,13 @@ public class DaoCliente implements IDaoCliente{
     public void removerCliente(Cliente cliente){
 
     }
+      @Override
+	public List<Cliente> buscarTodosCliente() {
+		
+		return listCliente;
+		
+		
+	}
 
 
 
